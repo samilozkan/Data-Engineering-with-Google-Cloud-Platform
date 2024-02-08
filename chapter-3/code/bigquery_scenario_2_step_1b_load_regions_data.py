399c4ee59d428bc5e496907c4bcfe4dd75e1954e
samilozkan <ssamilozkan@gmail.com>
2024-02-08 15:11:00 +0000

    updated project id v2

--- chapter-3/code/bigquery_scenario_2_step_1b_load_regions_data.py
+++ chapter-3/code/bigquery_scenario_2_step_1b_load_regions_data.py
@@ -17,17 +17,26 @@ from google.cloud import bigquery
 # TODO : Change to your project id
 PROJECT_ID = "pact-gcp-data-eng"
 PUBLIC_TABLE_ID = "bigquery-public-data.san_francisco_bikeshare.bikeshare_regions"
-                   #bigquery-public-data.san_francisco_bikeshare.bikeshare_regions#
 TARGET_TABLE_ID = "{}.raw_bikesharing.regions".format(PROJECT_ID)
 
+
+
+#def load_data_from_bigquery_public(PUBLIC_TABLE_ID, TARGET_TABLE_ID):
+#    client = bigquery.Client()
+#    job_config = bigquery.QueryJobConfig(
+#        destination = TARGET_TABLE_ID,
+#        write_disposition ='WRITE_TRUNCATE')
+
+
 def load_data_from_bigquery_public(PUBLIC_TABLE_ID, TARGET_TABLE_ID):
-    client = bigquery.Client()
+    client = bigquery.Client(project=PROJECT_ID)  # Projeyi belirtin
     job_config = bigquery.QueryJobConfig(
-        destination = TARGET_TABLE_ID,
-        write_disposition ='WRITE_TRUNCATE')
+        destination=TARGET_TABLE_ID,
+        write_disposition='WRITE_TRUNCATE')
 
     sql = "SELECT * FROM `{}`;".format(PUBLIC_TABLE_ID)
     query_job = client.query(sql, job_config=job_config)
+
 
     try:
         query_job.result()
@@ -37,3 +46,8 @@ def load_data_from_bigquery_public(PUBLIC_TABLE_ID, TARGET_TABLE_ID):
 
 if __name__ == '__main__':
     load_data_from_bigquery_public(PUBLIC_TABLE_ID, TARGET_TABLE_ID)
+
+
+############################################################################
+
+
